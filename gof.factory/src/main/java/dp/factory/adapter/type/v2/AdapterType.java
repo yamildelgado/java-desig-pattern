61746a2f4d453ab5cc5e725e5d8fecafa38ec246
Yamil <oracle@MacBook-Pro-de-APPLE.local>
2018-06-04 08:57:10 -0500

    Desig Pattern

---
+++ gof.factory/src/main/java/dp/factory/adapter/type/v2/AdapterType.java
@@ -0,0 +1,17 @@
+package dp.factory.adapter.type.v2;
+
+public enum AdapterType {
+	
+	
+	
+	JMS,
+	WS,
+	REST;
+	
+	private AdapterType() {
+		
+	}
+	
+	
+
+}
---
+++ gof.factory/src/main/java/dp/factory/adapter/v2/Adapter.java
@@ -0,0 +1,9 @@
+package dp.factory.adapter.v2;
+
+import dp.factory.core.v1.Message;
+
+public interface Adapter {
+
+	
+	public void send(Message msg) ;
+}
